846591c9412996e8e07553d364070ec6d6d11bcb
S. Rezvani <sharifrezvaniiii@gmail.com>
2022-05-23 12:43:22 +0430

    Done!

---
+++ second-assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SecondAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Determine the number of rows for the pyramid (1-9): ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= rows; i++)
+            {
+
+                for (int j = 0; j < rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int h = 1; h <= i; h++)
+                {
+                    Console.Write($"{h}");
+                }
+
+                for (int k = i - 1; k >= 1; k--)
+                {
+                    Console.Write($"{k}");
+                }
+                
+                Console.WriteLine("");
+
+            }
+        }
+    }
+}
---
+++ third-assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ThirdAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number = 0;
+            int sum = 0;
+
+            do
+            {
+                Console.WriteLine("Enter a natural number(1 and above):");
+                number = Convert.ToInt32(Console.ReadLine());
+            } while (number <= 0);
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"The sum of all the odd numbers between and including 1 and {number} is {sum}.");
+            
+        }
+    }
+}
