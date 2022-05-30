5f1d599a0f17836424822a67b932622fbdd0a64d
S. Rezvani <sharifrezvaniiii@gmail.com>
2022-05-30 21:36:28 +0430

    did 5th assignment

---
+++ fifth-assignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FifthAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<string> reoccurrences = new List<string>();
+
+            string givenNumberString;
+
+            string givenDigitString;
+
+
+            do
+            {
+                Console.WriteLine("Enter a number(must be more than two digits):");
+                givenNumberString = Console.ReadLine();
+            } while (givenNumberString.Length <= 1);
+
+
+            do
+            {
+                Console.WriteLine("Enter a single digit:");
+                givenDigitString = Console.ReadLine();
+            } while (givenDigitString.Length != 1);
+
+            
+            for (int i = 0; i < givenNumberString.Length; i++)
+            {
+                if (givenNumberString.Substring(i,1) == givenDigitString)
+                {
+                    reoccurrences.Add(givenDigitString);
+                }
+            }
+
+
+            Console.WriteLine($"\nThe given digit occurs {reoccurrences.Count} time/times in the given number.");
+            
+
+        }
+    }
+}
